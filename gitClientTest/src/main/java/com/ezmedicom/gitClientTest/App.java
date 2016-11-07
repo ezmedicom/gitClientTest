8c03030fdcddd0e24600b1250ffa7d4c18eb188f
sanghyun <sanghyun@sanghyun-PC>
2016-11-07 14:44:51 +0900

    프로젝트 생성 후 커밋

---
+++ gitClientTest/src/main/java/com/ezmedicom/gitClientTest/App.java
@@ -0,0 +1,13 @@
+package com.ezmedicom.gitClientTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
